c42ba03a242f4b32e3bb10210da7f004d59a687d
Jhesso <juho.hesso@hotmail.com>
2023-05-25 16:10:12 +0300

    implemented dardans player following/centering stuff

--- src/draw_map.c
+++ src/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 19:19:40 by jhesso            #+#    #+#             */
-/*   Updated: 2023/05/04 15:46:30 by jhesso           ###   ########.fr       */
+/*   Updated: 2023/05/25 16:09:31 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,50 @@ void	assign_image(t_game *game, char c, int x, int y)
 	}
 }
 
+static void	get_top_left_y(t_game *game, int *y)
+{
+	*y = 0;
+	if (game->player_y - 33 / 2 > 0)
+	{
+		if (game->map.rows - game->player_y > 32 / 2)
+			*y = game->player_y - 33 / 2;
+		else
+			*y = game->map.rows - 33;
+	}
+}
+
+/*	center_player()
+*	incase we get a big map (when drawn is more than 1920x1080)
+*	center the screen around the player
+*/
+static int	center_player(t_game *g, int row, int col, int x)
+{
+	int	y;
+
+	get_top_left_y(g, &y);
+	while (y < g->map.rows)
+	{
+		col = 0;
+		x = 0;
+		if (g->player_x - 60 / 2 > 0)
+		{
+			if (g->map.cols - g->player_x > 60 / 2)
+				x = g->player_x - 60 / 2;
+			else
+				x = g->player_x - 60;
+		}
+		while (col < g->map.cols)
+		{
+			assign_image(g, g->map.map[y][x], col * SIZE, row * SIZE);
+			col++;
+			x++;
+		}
+		row++;
+		y++;
+	}
+	return (1);
+}
+
 /*	draw_map()
 *	go through the map and put the corresponding sprite to the screen
 */
@@ -69,6 +113,9 @@ int	draw_map(t_game *game)
 	int	x;
 
 	y = 0;
+	if ((game->map.rows > 33 || game->map.cols > 60) && (game->player_y > 33 / 2
+			|| game->player_x > 60 / 2))
+		return (center_player(game, 0, 0, 0));
 	while (y < game->map.rows)
 	{
 		x = 0;
