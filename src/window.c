784f19a6a1831f410a3ae019ba967f9393d8da55
Jhesso <juho.hesso@hotmail.com>
2023-06-07 15:46:18 +0300

    removed player following attempt as well as window size limitations

--- src/window.c
+++ src/window.c
@@ -6,7 +6,7 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 16:12:58 by jhesso            #+#    #+#             */
-/*   Updated: 2023/06/07 15:28:35 by jhesso           ###   ########.fr       */
+/*   Updated: 2023/06/07 15:45:24 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,4 @@ void	calculate_window_size(t_game *game)
 {
 	game->width = game->map.cols * SIZE;
 	game->height = game->map.rows * SIZE;
-	// if (game->width > 1920)
-	// 	game->width = 1920;
-	// if (game->height > 1056)
-	// 	game->height = 1056;
 }
