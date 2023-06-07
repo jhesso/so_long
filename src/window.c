872f9bfab9a1f626f02ed0f0cee4c6876ee098ba
Jhesso <juho.hesso@hotmail.com>
2023-06-07 15:45:12 +0300

    player following attempt

--- src/window.c
+++ src/window.c
@@ -6,7 +6,7 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 16:12:58 by jhesso            #+#    #+#             */
-/*   Updated: 2023/05/04 16:01:34 by jhesso           ###   ########.fr       */
+/*   Updated: 2023/06/07 15:28:35 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ void	calculate_window_size(t_game *game)
 {
 	game->width = game->map.cols * SIZE;
 	game->height = game->map.rows * SIZE;
-	if (game->width > 1920)
-		game->width = 1920;
-	if (game->height > 1056)
-		game->height = 1056;
+	// if (game->width > 1920)
+	// 	game->width = 1920;
+	// if (game->height > 1056)
+	// 	game->height = 1056;
 }
